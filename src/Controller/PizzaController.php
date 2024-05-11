55b014ff1ae0f4285147c126c7288edc090dceb0
inforob <inforob@ono.com>
2024-05-11 16:53:42 +0200

    6th commit

--- src/Controller/PizzaController.php
+++ src/Controller/PizzaController.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use App\Command\Email\EmailCommand;
-use App\Entity\Email;
 use App\Repository\EmailRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Attribute\Route;
@@ -18,17 +20,23 @@ class PizzaController extends AbstractController
 
     }
     #[Route('/send', name: 'send_emails', options: ['expose'=>true], methods: ["POST"])]
-    public function index(EmailRepository $emailRepository): JsonResponse
+    public function index(Request $request,EmailRepository $emailRepository): JsonResponse
     {
+          $emailsIdForSend = $request->request->all();
+          if(!empty($emailsIdForSend)){
+                foreach ($emailsIdForSend['email'] as $emailId) {
+                    $emailStored = $emailRepository->find(intval($emailId));
 
+                    if(!$emailStored->isSended()) {
+                        $this->commandBus->dispatch(new EmailCommand($emailStored));
+                    }
+                }
 
-//        foreach ($emailsNotSended as $email) {
-//
-//            $this->commandBus->dispatch(new EmailCommand($email));
-//        }
+                return $this->prepareResponseOk();
 
-        return $this->json(['code'=> 200,'status'=>'server:ok']);
+          }
 
+          return $this->prepareResponseEmpty();
     }
 
     #[Route('/list', name: 'list_emails')]
@@ -36,6 +44,22 @@ class PizzaController extends AbstractController
     {
         return $this->render('emails/list.html.twig', [
             'emails' => $emailRepository->findAll()
+        ]);
+    }
+
+    private function prepareResponseOk(): JsonResponse
+    {
+        return $this->json([
+            'code'=> 200,
+            'status'=>'server:ok'
+        ]);
+    }
+
+    private function prepareResponseEmpty(): JsonResponse
+    {
+        return $this->json([
+            'code'=> 300,
+            'status'=>'server:ok'
         ]);
     }
 }
