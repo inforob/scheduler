5f5ae49139957ad8cedeaa79d3f24d9dce7c4674
inforob <inforob@ono.com>
2024-06-02 17:45:12 +0200

    11th commit

--- src/Scheduler/EmailScheduler.php
+++ src/Scheduler/EmailScheduler.php
@@ -2,23 +2,32 @@
 
 declare(strict_types=1);
 
-// src/Scheduler/SaleTaskProvider.php
+
 namespace App\Scheduler;
 
 use App\Command\EmailScheduled\SendEmailDaily;
+use Symfony\Component\Lock\LockFactory;
 use Symfony\Component\Scheduler\Attribute\AsSchedule;
 use Symfony\Component\Scheduler\RecurringMessage;
 use Symfony\Component\Scheduler\Schedule;
 use Symfony\Component\Scheduler\ScheduleProviderInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 
-#[AsSchedule]
+#[AsSchedule(name:"email")]
 class EmailScheduler implements ScheduleProviderInterface
 {
+    public function __construct(
+        private readonly CacheInterface $cache,
+        private readonly LockFactory $lockFactory
+    )
+    {}
     public function getSchedule(): Schedule
     {
         return $this->schedule ??= (new Schedule())
             ->with(
-                RecurringMessage::every('15 seconds', new SendEmailDaily((int)uniqid()))
-            );
+                RecurringMessage::every('10 seconds', new SendEmailDaily((int)uniqid()))
+            )
+            ->lock($this->lockFactory->createLock('scheduler-email'))
+            ->stateful($this->cache);
     }
 }
